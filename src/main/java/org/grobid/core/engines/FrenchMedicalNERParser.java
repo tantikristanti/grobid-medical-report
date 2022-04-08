e9a7a35d670ff00ad16eb42c28ece131223a5361
tkristan <tkristan@MP16-PRO.local>
2022-04-08 16:50:15 +0200

    fix minor bugs when labelling

--- src/main/java/org/grobid/core/engines/FrenchMedicalNERParser.java
+++ src/main/java/org/grobid/core/engines/FrenchMedicalNERParser.java
@@ -1322,18 +1322,17 @@ public class FrenchMedicalNERParser extends AbstractParser {
         boolean result = false;
         if ((s1.equals(field)) || (s1.equals("I-" + field))) {
             result = true;
-            if ((s1.equals("<other>") || s1.equals("I-<other>"))) {
+            if (s1.equals(lastTag0) || (s1).equals("I-" + lastTag0)) { // if current tag is the same, just concatenate the string
                 if (addSpace)
                     buffer.append(" ").append(s2);
                 else
                     buffer.append(s2);
-            } else if (s1.equals(lastTag0) || (s1).equals("I-" + lastTag0)) { // if current tag is the same, just concatenate the string
+            } else {
                 if (addSpace)
-                    buffer.append(" ").append(s2);
+                    buffer.append(" ").append(outField).append(s2);
                 else
-                    buffer.append(s2);
-            } else
-                buffer.append(outField).append(s2); // otherwise, add the current label with the concatenated string
+                    buffer.append(outField).append(s2); // otherwise, add the current label with the concatenated string
+            }
         }
         return result;
     }
